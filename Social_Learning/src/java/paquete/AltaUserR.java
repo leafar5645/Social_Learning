6c081e0734f99e95168e7e88dcb1512003cc480a
Rafael Hernandez Ruiz <rafassassinscreed@gmail.com>
2018-05-15 15:09:41 -0500

    Enviar correo en el registro

--- Social_Learning/src/java/paquete/AltaUserR.java
+++ Social_Learning/src/java/paquete/AltaUserR.java
@@ -6,6 +6,7 @@
 package paquete;
 
 //import static com.sun.javafx.scene.control.skin.Utils.getResource;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,18 +25,35 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import static java.lang.ProcessBuilder.Redirect.to;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.MessageDigest;
+import java.util.Properties;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.BodyPart;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.jcp.xml.dsig.internal.dom.Utils;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  *
@@ -61,11 +79,13 @@ public class AltaUserR extends HttpServlet {
 
    
 
-       
+      // MessageDigest md = null; 
+
      
      int res=0;
      Usuario userA = new Usuario();
      res=userA.AltaUR(Correo, nombre, contra, tipo );
+
      if(res==1)
      {
           File file = new File(path+"/" + userA.getId()+ ".jpg" ); //La clase file tiene 3 constructores  File (String path).  Crea una nueva instancia de tipo file  convirtiendo la cadena de nombre de ruta dada en una ruta de acceso abstracta.
@@ -73,6 +93,84 @@ public class AltaUserR extends HttpServlet {
       
     InputStream input = filePart.getInputStream(); //Obtener el contenido de la parte en un inputStream
         Files.copy(input, file.toPath(),StandardCopyOption.REPLACE_EXISTING );
+        
+        /*
+        
+     
+        */
+            final String miCorreo = "social.learning.2018@gmail.com"; //correo del administrador desde que seran enviados los mensajes
+    final String miContraseña = "Social9222"; //contraseña del correo
+ 
+    String servidorSMTP = "smtp.gmail.com";
+   String puertoEnvio = "587";
+    String mailReceptor = Correo;
+    String asunto = "Registro"; 
+    String cuerpo = "Bienvenido al sistema"; //mensaje que sera enviado en el correro
+  
+               Properties props = new Properties();
+        props.put("mail.smtp.user", miCorreo);
+        props.put("mail.smtp.host", servidorSMTP);
+        props.put("mail.smtp.port", puertoEnvio);
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.port", puertoEnvio);
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
+     
+        //SecurityManager security = System.getSecurityManager();
+
+        try {
+           // Authenticator auth = new autentificadorSMTP();
+            
+            
+            
+            Session session;
+       session = Session.getInstance(props, 
+               new javax.mail.Authenticator() {
+                   
+                   
+                   
+                   
+                   @Override
+                   protected PasswordAuthentication getPasswordAuthentication()  {
+                       
+                       
+                       
+                       return new PasswordAuthentication(miCorreo,miContraseña);
+                       
+                       
+                       
+                   }
+                   
+               });
+
+ 
+
+            session.setDebug(true);
+
+            MimeMessage msg = new MimeMessage(session);
+            msg.setText(cuerpo);
+            msg.setSubject(asunto);
+            msg.setFrom(new InternetAddress(miCorreo));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+                    mailReceptor));
+         
+            
+            Transport.send(msg, msg.getAllRecipients());
+            
+        } catch (Exception mex) {
+        }
+   
+        
+    
+        
+        
+        
+        
+        
+        
          
           PrintWriter out = response.getWriter();
          response.setContentType("text/html;charset=UTF-8");
@@ -84,7 +182,7 @@ out.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/limonte-sweeta
        out.println(" swal({\n" +
 "  title: 'exito',\n" +
 "  text: \"Alta exitosa !\",\n" +
-"  type: 'correct',\n" +
+"  type: 'success',\n" +
 "  showCancelButton: false,\n" +
 "  confirmButtonColor: '#d33',\n" +
 "  cancelButtonColor: '#d33',\n" +
