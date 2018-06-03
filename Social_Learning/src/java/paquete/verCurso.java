b7a3818a39fe7bac8fc9ab30c76d703a1440bf42
Rafael Hernandez Ruiz <rafassassinscreed@gmail.com>
2018-06-02 21:17:03 -0500

    La base con mis registros

--- Social_Learning/src/java/paquete/verCurso.java
+++ Social_Learning/src/java/paquete/verCurso.java
@@ -23,7 +23,7 @@ public class verCurso extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session= request.getSession();
-        Usuario user =(Usuario) session.getAttribute("AlumnoH");
+        Usuario user =(Usuario) session.getAttribute("AlumnoR");
         ArrayList<Curso> cursos = user.getCursos();
         String sid;
         sid = request.getParameter("id");
